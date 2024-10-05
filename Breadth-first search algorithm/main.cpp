a633fa950e82e2abbba63e655a9361889c71a404
Евгений Шкумат <kotbmasle@gmail.com>
2024-10-05 11:36:01 +0300

    getting path to n vertex

--- Breadth-first search algorithm/main.cpp
+++ Breadth-first search algorithm/main.cpp
@@ -4,7 +4,8 @@
 #define INF 2147483647
 using namespace std;
 
-vector<int> bfs(vector<vector<int>>, vector<int>&, int);
+vector<int> bfs(vector<vector<int>>, vector<int>&, vector<int>&, int);
+vector<int> get_path(vector<int>, int);
 
 int main() {
 	int vertex_count, edge_count, start;
@@ -26,8 +27,10 @@ int main() {
 
 	cout << "Enter start vertex: ";
 	cin >> start;
+
 	vector<int> names(graph.size());
-	vector<int> distances = bfs(graph, names, start);
+	vector<int> from(graph.size(), -1);
+	vector<int> distances = bfs(graph, names, from, start);
 	
 	for (int i : names)
 	{
@@ -46,10 +49,19 @@ int main() {
 		}
 	}
 
+	vector<int> path = get_path(from, 4);
+
+	cout << "------------------------------\nPath\n";
+
+	for (int i : path)
+	{
+		cout << i << " ";
+	}
+
 	return 0;
 }
 
-vector<int> bfs(vector<vector<int>> graph, vector<int>& names,int start) {
+vector<int> bfs(vector<vector<int>> graph, vector<int>& names, vector<int>& from, int start) {
 	vector<int> distance(graph.size(), INF);
 	queue<int> q;
 
@@ -64,12 +76,25 @@ vector<int> bfs(vector<vector<int>> graph, vector<int>& names,int start) {
 			if (distance[to] > distance[v] + 1) {
 				distance[to] = distance[v] + 1;
 				names[to] = to;
+				from[to] = v;
 				q.push(to);
 			}
 		}
 	}
 
 	return distance;
+}
+
+vector<int> get_path(vector<int> from, int finish) {
+	vector<int> path;
+	for (int i = finish; i != -1; i = from[i])
+	{
+		path.push_back(i);
+	}
+
+	reverse(path.begin(), path.end());
+
+	return path;
 }
 
 	
